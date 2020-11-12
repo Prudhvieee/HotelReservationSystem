fc7d0fa090486d7556eea91cfb7b0f1e7b53cf5c
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-12 15:22:04 +0530

    Add project files.

---
+++ HotelReservationSystem/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HotelReservationSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hotel Reservation Program\n");
+        }
+    }
+}
