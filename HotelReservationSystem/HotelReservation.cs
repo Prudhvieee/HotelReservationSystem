8c5daf835ecd847ab1a16315960228f1e62f31e2
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-12 19:53:32 +0530

    [Prudhvi] add . adding ability to find cheapset hotel

--- HotelReservationSystem/HotelReservation.cs
+++ HotelReservationSystem/HotelReservation.cs
@@ -5,19 +5,62 @@ namespace HotelReservationSystem
 {
     public class HotelReservation
     {
+        ///creating a list, it contains hotel names and prices
+        List<HotelDetail> hotelList = new List<HotelDetail>();
+        SortedDictionary<int, string> sortByPrice = new SortedDictionary<int, string>();
+        ///UC-1
+        ///AddHotels method is used to add hotels into list
         public void AddHotels()
         {
-            ///creating a list, it contains hotel names and prices
-            List<HotelDetail> hotelList = new List<HotelDetail>();
             ///Addding hotels to list
             hotelList.Add(new HotelDetail("LakeWood", 110));
             hotelList.Add(new HotelDetail("BridgeWood", 160));
-            hotelList.Add(new HotelDetail("RidgeWood", 160));
+            hotelList.Add(new HotelDetail("RidgeWood", 220));
             Console.WriteLine("Hotels are added into the list");
             ///Displaying added hotels into list
-            foreach (var item in hotelList)
+            foreach (var hotels in hotelList)
             {
-                Console.WriteLine(item.hotelName+" "+item.weekdayRegularRates);
+                Console.WriteLine(hotels.hotelName + " " + hotels.weekdayRegularRates);
+            }
+        }
+        /// <summary>
+        /// calculatingHotelPrices method is used to calculate hotel price
+        /// </summary>
+        /// <param name="checkInDate"></param>
+        /// <param name="checkOutDate"></param>
+        public void calculatingHotelPrices(DateTime checkInDate, DateTime checkOutDate)
+        {
+            if (checkInDate < checkOutDate)
+            {
+                TimeSpan stayingPeriod = checkOutDate.Subtract(checkInDate);
+                foreach (var hotels in hotelList)
+                {
+                    int total = 0;
+                    for (int i = 0; i <= stayingPeriod.TotalDays; i++)
+                    {
+                        total = total + hotels.weekdayRegularRates;
+                    }
+                    Console.WriteLine("Hotel Name : {0} and Total Price : {1}", hotels.hotelName, total);
+                    sortByPrice.Add(total, hotels.hotelName);
+                }
+            }
+            else
+            {
+                Console.WriteLine("enter the dates properly");
+            }
+        }
+        /// <summary>
+        /// cheapestHotel method is used to find the cheapest hotel among the present hotels.
+        /// </summary>
+        /// <param name="checkInDate"></param>
+        /// <param name="checkOutDate"></param>
+        public void CheapestHotel(DateTime checkInDate, DateTime checkOutDate)
+        {
+            calculatingHotelPrices(checkInDate, checkOutDate);
+            foreach (var hotelPrice in sortByPrice)
+            {
+                Console.WriteLine("Cheapest Hotel Name : {0} , Total Price : {1}", hotelPrice.Value, hotelPrice.Key);
+                break;
             }
         }
     }
--- HotelReservationSystem/Program.cs
+++ HotelReservationSystem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 namespace HotelReservationSystem
 {
     public class Program
@@ -8,6 +9,13 @@ namespace HotelReservationSystem
             Console.WriteLine("Welcome to Hotel Reservation Program\n");
             HotelReservation hotelReservation = new HotelReservation();
             hotelReservation.AddHotels();
+            Console.WriteLine("Enter the check-in date informat dd/mm/yyyy");
+            string checkIn = Console.ReadLine();
+            DateTime checkInDate = DateTime.ParseExact(checkIn, "dd/mm/yyyy", CultureInfo.InvariantCulture);
+            Console.WriteLine("Enter the check-out date informat dd/mm/yyyy");
+            string checkOut = Console.ReadLine();
+            DateTime checkOutDate = DateTime.ParseExact(checkOut,"dd/mm/yyyy",CultureInfo.InvariantCulture);
+            hotelReservation.CheapestHotel(checkInDate, checkOutDate);
         }
     }
 }
